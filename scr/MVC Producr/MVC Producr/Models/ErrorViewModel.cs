8d746b9dcbef845d5fa8c977a057d1eac8600622
BegzodDev <begzodpublic@gmail.com>
2023-03-13 13:58:40 +0500

    added file to git

---
+++ scr/MVC Producr/MVC Producr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Producr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
