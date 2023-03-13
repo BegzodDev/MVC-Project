218092240d60ab550660084b610ba3dbf5e2acc0
BegzodDev <begzodpublic@gmail.com>
2023-03-13 17:07:45 +0500

    ADded Usercontroller admin product and Views for user created register for user and get products

---
+++ scr/MVC Producr/MVC Producr/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Producr.Models.Domain.Entities;
+
+namespace MVC_Producr.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<User> Users { get; set; } 
+        public DbSet<Product> Products { get; set; }
+
+       
+    }
+
+}
